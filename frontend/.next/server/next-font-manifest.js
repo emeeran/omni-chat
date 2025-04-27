8c6aea1f32ce7f399d5249aa839faeaca76bf8bc
Meeran <emeeran@hotmail.com>
2025-04-27 21:17:07 +0530

    Remove Next.js build files from Git

--- frontend/.next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/home/em/code/wip/omni-chat/frontend/src/app/layout\":[\"static/media/a34f9d1faa5f3315-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
--- frontend/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"udE67apmdjrN7OKblELchTqm11QWcSYWs3ARYPt8t9w=\"\n}"
